458ea2dfadeee9a599f7d514110e9e2a1b219146
wshan22 <44222898+wshan22@users.noreply.github.com>
2018-11-15 17:46:11 +0900

    Add files via upload

---
+++ ex.php
@@ -0,0 +1,15 @@
+﻿<?php
+    $myfile = fopen("names.txt", "w");
+
+    $txt = "John\n";
+    fwrite($myfile, $txt);
+    $txt = "David\n";
+    fwrite($myfile, $txt);
+
+    fclose($myfile);
+
+    $read = file('names.txt');
+    foreach ($read as $line) {
+        echo $line .", ";
+    }
+?>
